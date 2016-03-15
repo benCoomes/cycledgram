5d6ef947d9ad09cbb733c8fa4438843acb901271
Ben Coomes <bcoomes@g.clemson.edu>
2016-03-15 17:11:03 -0400

    Put in planning comments, structure is now defined. Next, implement inst subclasses

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-class Reg(object):
-
-    def __init__(self, num, isBusy=False):
-        self.num = num
-        self.isBusy = isBusy
+import Instruction
+import Registers
 
 
 # main method start
 # ----------------------------------------------
 
-registers = []
 
-for i in range(0, 13):
-    nextreg = Reg(i, False)
-    registers.append(nextreg)
+# make cpu 
+"""
+cpu object holds all instructions. 
+cpu.addInst(Inst) adds an instruction to the instruction list
+cpu.tick() runs one cpu clock cycle (described in my notes)
+cpu.printPipeline() prints a history of executed instructions and 
+the order that they were processed in the pipeline. (output required
+for project). 
+"""
 
-for r in registers:
-    print "R%d busy? : %d" % (r.num, r.isBusy)
-
-
+# process input to generate instructions
+# need to make a set of registers to give instructions as they are made
+regs = Registers.RegisterSet(10)
+# read in lines, parse as instructions
 print("Enter commands below, enter an empty line when finished")
 raw_inst = []
 next_line = raw_input();
@@ -27,12 +29,74 @@ while(next_line != ''):
     raw_inst.append(next_line)
     next_line = raw_input()
 
+inst_list = []
 for s in raw_inst:
-    print s
+    parts = s.split()
+    if(len(parts) == 2):
+        parts[1] = parts[1].split(',')
+    else:
+        print "Error: bad string format for instruction input"
 
-inst = []
-for s in raw_inst:
-    i = getInstruction(s)
+    print parts
+
+    if(parts[0] == 'add'):
+    #make add
+        inst_regs = []
+        # uncheked assumption
+        for r in parts[1]:
+            r_num = int(r[1])
+            inst_regs.append(regs.get(r_num))
+        inst_list.append(Add_inst(inst_regs))
+        #cpu.addInst(Add_inst(inst_regs))
+
+    elif(parts[0] == 'sub'):
+        #make sub
+        inst_regs = []
+        # uncheked assumption
+        for r in parts[1]:
+            print r
+            r_num = int(r[1])
+            inst_regs.append(regs.get(r_num))
+        inst_list.append(Sub_inst(inst_regs))
+        #cpu.addInst(Sub_inst(inst_regs))
+
+    elif(parts[0] == 'lw'):
+        #make lw
+        inst_regs = []
+        inst_regs.append(regs.get(int(parts[1][1])))
+        #assumption about how offset works: 
+        # offset + regnum = actual reg num
+        # probably wrong, CHECK THIS
+        inst_regs.append(regs.get(int(parts[2][1]) + int(parts[2][3])))
+
+        inst_list.append(Load_inst(inst_regs))
+        #cpu.addInst(Load_inst(inst_regs))   
+
+    elif(parts[0] == 'sw'):
+        #make sw
+        inst_regs = []
+        inst_regs.append(regs.get(int(parts[1][1])))
+        #assumption about how offset works: 
+        # offset + regnum = actual reg num
+        # probably wrong, CHECK THIS
+        inst_regs.append(regs.get(int(parts[2][1]) + int(parts[2][3])))
+        inst_list.append(Store_inst(inst_regs))
+        #cpu.addInst(Store_inst(inst_regs))
+
+    else:
+        print "ERROR! Unrecognized instruction"
+#create instance of a subclass of instruction (add, lw, sub, sw)
+#add the created inst to the cpu list
+
+
+#run the cpu while there are still unexecuted instructions
+# cpu.unex_inst keeps track of this
+
+#when all inst have executed, print pipeline
+
+
+
+
 
 
 
